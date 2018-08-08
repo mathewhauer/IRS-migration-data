d41e3dce411644c222a34e9837057579d84929be
Matthew Hauer <mhauer@cosspp.edu>
2018-08-08 13:26:48 -0400

    Added the readme.

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# IRS-migration-data
-IRS migration data gather
+# Introduction
+Here I provide the code for:  [**IRS County-to-County Migration Data**](https://github.com/mathewhauer/IRS-migration-data/blob/master/manuscript_file.pdf)
+
+### Citation
+
+
+> Hauer, M.E. *Open Science Framework.* (DOI Forthcoming) (YEAR ACCESSED).
+
+
+
+# Abstract
+
+**BACKGROUND**: The Internal Revenue Service's (IRS) county-to-county migration data are an incredible resource for understanding migration in the United States. Produced annually since 1990 in conjunction with the US Census Bureau, the IRS migration data represent 95 to 98 percent of the tax filing universe and their dependents, making the IRS migration data one of the largest sources of migration data. However, any analysis using the IRS migration data must wrangle at least seven legacy formats of these public data across more than 2000 data files -- a serious burden for migration research. 
+
+**OBJECTIVE**: To produce a single, flat data file containing complete county-to-county IRS migration flow data. 
+
+**METHODS**: This paper uses R to wrangle more than 2,000 IRS migration files into a single, flat data file for use in migration research. 
+
+**CONTRIBUTION**: To encourage and facilitate the use of this data, I provide the full R script to download and flatten the IRS migration data as counts and a finalized data set covering the period 1990-2010.
+
+
+# Organization
+- `LATEX`  — Contains latex files to reproduce the main manuscript file.
+- `MigData`  — Initial data resources, unprocessed.
+- `DATA-PROCESSED` — Final, post-processed data.
+
+# Use
+- Feel free to create a new branch for further incorporation and analysis. 
+- More information in is located in each folder `DATA`.
+
+# Data
+
+The final IRS migration data can be reproduced by running the `county_county_migration_matt.r` file.
+
+
+
+# Codebook
+
+`ORIGIN`
+- Refers to the 5-digit FIPS code for the origin of the migrants.
+
+`DESTINATION`
+- Refers to the two-digit FIPS code associated with each state.
+
+`1990:2010`
+- Refers to number of migrants who moved from ORIGIN to DESTINATION in a given year.
+
+- NOTE: there is an additional 5-digit FIPS code in the both the ORIGIN and DESTINATION field. This is coded as 99999 and is in reference to all migration flows containing less than 10 tax filers.
+
+# Correspondence
+For any issues with the functionality of these scripts please [create an issue](https://github.com/mathewhauer/IRS-migration-data/issues).
+
+## License
+The data collected and presented is licensed under the [Creative Commons Attribution 3.0 license](http://creativecommons.org/licenses/by/3.0/us/deed.en_US), and the underlying code used to format, analyze and display that content is licensed under the [MIT license](http://opensource.org/licenses/mit-license.php).
--- county_county_migration_matt.r
+++ county_county_migration_matt.r
@@ -59,56 +59,56 @@ us_states <- us_states %>%
 ###
 ### Uncoment the following block of code to download and unzip the IRS migration data.
 ###
-## Setting the first set of years to download. 1990-2003 are in the same format. 2004- are in a second format
-# years <- c('1990to1991',
-#            '1991to1992',
-#            '1992to1993',
-#            '1993to1994',
-#            '1994to1995',
-#            '1995to1996',
-#            '1996to1997',
-#            '1997to1998',
-#            '1998to1999',
-#            '1999to2000',
-#            '2000to2001',
-#            '2001to2002',
-#            '2002to2003',
-#            '2003to2004'
-# )
-# 
-# for (year in years){
-# 
-#   download.file(paste0("https://www.irs.gov/pub/irs-soi/",year,"countymigration.zip"), # download the IRS migration data
-#                 dest=paste0("MigData/",year,"countymigration.zip"), # destination is MigData/
-#                 mode="wb")
-#   unzip(paste0("MigData/", year, "countymigration.zip"), exdir = paste0("./MigData/", year))
-# }
-#
-## Setting the second set of years to download. 
-# years <- c('0405',
-#            '0506',
-#            '0607',
-#            '0708',
-#            '0809',
-#            '0910',
-#            '1011'
-# )
-# for (year in years){
-#   download.file(paste0("https://www.irs.gov/pub/irs-soi/county",year,".zip"), # download the IRS migration data
-#                 dest=paste0("MigData/",year,"countymigration.zip") # destination is MigData/
-#                 , mode="wb")
-#   unzip(paste0("MigData/", year, "countymigration.zip"), exdir = paste0("./MigData/", year))
-# }
-# 
-### The following IRS files do not contain data and cannot be read.
-# unlink("MigData/1998to1999/1998to1999CountyMigration/1998to1999CountyMigrationInflow/co989usi.xls") # file cannot be read and contains no information
-# unlink("MigData/1998to1999/1998to1999CountyMigration/1998to1999CountyMigrationOutflow/co989uso.xls")
-# unlink("MigData/1999to2000/1999to2000CountyMigration/1999to2000CountyMigrationInflow/co990usi.xls")
-# unlink("MigData/1999to2000/1999to2000CountyMigration/1999to2000CountyMigrationOutflow/co990uso.xls")
-# unlink("MigData/2000to2001/2000to2001CountyMigration/2000to2001CountyMigrationInflow/co001usir.xls")
-# unlink("MigData/2000to2001/2000to2001CountyMigration/2000to2001CountyMigrationOutflow/co001usor.xls")
-# unlink("MigData/2001to2002/2001to2002CountyMigration/2001to2002CountyMigrationInflow/co102usi.xls")
-# unlink("MigData/2001to2002/2001to2002CountyMigration/2001to2002CountyMigrationOutflow/co102uso.xls")
+# Setting the first set of years to download. 1990-2003 are in the same format. 2004- are in a second format
+years <- c('1990to1991',
+           '1991to1992',
+           '1992to1993',
+           '1993to1994',
+           '1994to1995',
+           '1995to1996',
+           '1996to1997',
+           '1997to1998',
+           '1998to1999',
+           '1999to2000',
+           '2000to2001',
+           '2001to2002',
+           '2002to2003',
+           '2003to2004'
+)
+
+for (year in years){
+
+  download.file(paste0("https://www.irs.gov/pub/irs-soi/",year,"countymigration.zip"), # download the IRS migration data
+                dest=paste0("MigData/",year,"countymigration.zip"), # destination is MigData/
+                mode="wb")
+  unzip(paste0("MigData/", year, "countymigration.zip"), exdir = paste0("./MigData/", year))
+}
+
+# Setting the second set of years to download.
+years <- c('0405',
+           '0506',
+           '0607',
+           '0708',
+           '0809',
+           '0910',
+           '1011'
+)
+for (year in years){
+  download.file(paste0("https://www.irs.gov/pub/irs-soi/county",year,".zip"), # download the IRS migration data
+                dest=paste0("MigData/",year,"countymigration.zip") # destination is MigData/
+                , mode="wb")
+  unzip(paste0("MigData/", year, "countymigration.zip"), exdir = paste0("./MigData/", year))
+}
+
+## The following IRS files do not contain data and cannot be read.
+unlink("MigData/1998to1999/1998to1999CountyMigration/1998to1999CountyMigrationInflow/co989usi.xls") # file cannot be read and contains no information
+unlink("MigData/1998to1999/1998to1999CountyMigration/1998to1999CountyMigrationOutflow/co989uso.xls")
+unlink("MigData/1999to2000/1999to2000CountyMigration/1999to2000CountyMigrationInflow/co990usi.xls")
+unlink("MigData/1999to2000/1999to2000CountyMigration/1999to2000CountyMigrationOutflow/co990uso.xls")
+unlink("MigData/2000to2001/2000to2001CountyMigration/2000to2001CountyMigrationInflow/co001usir.xls")
+unlink("MigData/2000to2001/2000to2001CountyMigration/2000to2001CountyMigrationOutflow/co001usor.xls")
+unlink("MigData/2001to2002/2001to2002CountyMigration/2001to2002CountyMigrationInflow/co102usi.xls")
+unlink("MigData/2001to2002/2001to2002CountyMigration/2001to2002CountyMigrationOutflow/co102uso.xls")
 # #############
 # #############
 
@@ -1076,6 +1076,6 @@ county_migration_probs <- gather(county_migration_data, Year, mig, `1990`:`2010`
   spread(Year, freq) # going from tall to wide
 
 ########## Export Dataset ##########
-# write.table(county_migration_data, "county_migration_data.txt", sep = "\t", row.names = FALSE) # Tab Delimited
+# write.table(county_migration_data, "DATA-PROCESSED/county_migration_data.txt", sep = "\t", row.names = FALSE) # Tab Delimited
 
 q(save = "no") # Quit R (No Save)
